ba5625800a16036b9fcaa6d33e3751c8e8cbeae8
tjd <tjduigna@buffalo.edu>
2016-01-09 18:21:47 -0500

    Initial implementation of read_pdb with numpy record arrays.

--- atomic/pdb.py
+++ atomic/pdb.py
@@ -40,18 +40,54 @@ def read_pdb(path):
             flins = _req.get(url).text.splitlines()
         except:
             ConnectionError('{}: check that url exists.'.format(url))
-    reccnt = _pre_process_pdb(flins)
+    rds = _pre_process_pdb(flins)       # Get number of occurances of records
+    sepdict, errors = _parse_pdb(flins, rds)    # dictionary separated into records
+    return sepdict, errors
+
 
 def _pre_process_pdb(flins):
     recdims = {}
+    reccnt = [line[:6].strip() for line in flins]
     for record in records:
-        recdims[key] = sum([1 for rec in recs if record == rec])
+        if 'ORIGX' in record or 'SCALE' in record \
+        or 'MATRX' in record or 'DBREF' in record:
+            recdims[record[:5]] = [sum([1 for rec in reccnt if record == rec]), 0]
+        else:
+            recdims[record] = [sum([1 for rec in reccnt if record == rec]), 0]
+    print(recdims['ATOM'])
+    print(recdims['HETATM'])
+    print(recdims['ANISOU'])
+    return recdims
 
-
-
-def _parse_pdb(flins):
-
-    pass
+def _parse_pdb(flins, rds):
+    sepdict = {}
+    errors = []
+    for key, val in records.items():
+        trec = val['rec']
+        if trec:
+            dtyp=[(i[2], i[3]) for i in trec]
+            sepdict[key] = np.empty((rds[key][0], ), dtype=dtyp)
+    for i, line in enumerate(flins):
+        rec = line[:6].strip()
+        if 'ORIGX' in rec or 'SCALE' in rec \
+        or 'MATRX' in rec or 'DBREF' in rec:
+            rec = rec[:5]
+        if records[rec]['rec']:
+            slices = [(i[0], i[1]) for i in records[rec]['rec']]
+            dtyps = [(i[2], i[3]) for i in records[rec]['rec']]
+            if rec in sepdict:
+                cidx = rds[rec][1]
+                print(rec)
+                print(line)
+                print(slices)
+                print(dtyps)
+                #try:
+                sepdict[rec][cidx] = tuple([line[i:j] for i, j in slices])
+                #except (ValueError, TypeError) as e:
+                #    errors.append('{}:{}'.format(rec, e))
+                #    pass
+                rds[rec][1] += 1
+    return sepdict, errors
 
 
 
