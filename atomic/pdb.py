f24611017f292d66b28e5eea523d3eba9b4d44af
tjd <tjduigna@buffalo.edu>
2016-01-09 23:16:58 -0500

    read_pdb more in line with logic in read_xyz using _gen_fdf.

--- atomic/pdb.py
+++ atomic/pdb.py
@@ -10,6 +10,7 @@ from io import StringIO
 from warnings import warn as _warn
 
 #Hacky imports
+from atomic.algorithms.nonjitted import generate_minimal_framedf_from_onedf as _gen_fdf
 sys.path.insert(0, '/home/tjd/Programs/analytics-exa/exa')
 from exa.utils import mkpath
 
@@ -24,7 +25,7 @@ def read_pdb(path):
     Reads a PDB file
 
     Args
-        path (str): file path (local) or PDB code (remote)
+        path (str): file path (local) or PDB identifier (remote)
 
     Return
         unikws (dict): dataframes containing 'frame' and 'one' body data
@@ -42,20 +43,15 @@ def read_pdb(path):
         except:
             ConnectionError('{}: check that url exists.'.format(url))
     rds = _pre_process_pdb(flins)
-    odx = [i for i range(rds['nat'])]
-    frdx = [i for j in range(rds['nrf']) for i in j]
-    print('odx', odx)
-    print('frdx', frdx)
+    frdx = [i for i in range(rds['nrf']) for j in range(rds['nat'])]
+    odx = [i for j in range(rds['nrf']) for i in range(rds['nat'])]
     sepdict = _parse_pdb(flins, rds)
-    #frame = pd.DataFrame.from_dict(fdict)
-    #fidxs = [e for e, j in enumerate(frame.values) for i in range(j)]
-    #oidxs = [i for j in frame.values for i in range(j)]
-    one = sepdict['ATOM'][['symbol', 'x', 'y', 'z']]
-    #one.loc[:, 'one'] = oidxs
-    #one.loc[:, 'frame'] = fidxs
-    #one.set_index(['frame', 'one'], inplace=True)
-    #return {'frame': frame, 'one': one}
-    return one
+    one = sepdict['ATOM'].loc[:, ['symbol', 'x', 'y', 'z']]
+    one['frame'] = frdx
+    one['one'] = odx
+    one.set_index(['frame', 'one'], inplace=True)
+    frame = _gen_fdf(one)
+    return {'frame': frame, 'one': one}
 
 def _pre_process_pdb(flins):
     rds = {}
@@ -67,12 +63,9 @@ def _pre_process_pdb(flins):
     else:
         nrf = 1
     nat = rds['ATOM'][0] // nrf
-    #fdict = {'count': np.empty((nrf,), dtype='i8')}
-    #for i in range(nrf):
-    #    fdict['count'][i] = nat
     rds['nrf'] = nrf
     rds['nat'] = nat
-    return rds#, fdict
+    return rds
 
 def _parse_pdb(flins, rds):
     sepdict = {}
@@ -81,18 +74,18 @@ def _parse_pdb(flins, rds):
         #if 'ORIGX' in rec or 'SCALE' in rec \
         #or 'MATRX' in rec or 'DBREF' in rec:
         #    rec = rec[:5]
-        #if rec == 'ATOM' or rec == 'HETATM' or rec == 'ANISOU':
-        if records[rec]['rec']:
-            sepdict.setdefault(
-                rec, {i[2]: np.zeros(
-                    (rds[rec][0], ), dtype=i[3]
-                ) for i in records[rec]['rec']}
-            )
-            slices = [(i[2], i[0], i[1]) for i in records[rec]['rec']]
-            for sub, i, j in slices:
-                if line[i:j].strip():
-                    sepdict[rec][sub][rds[rec][1]] = line[i:j]
-            rds[rec][1] += 1
+        if rec == 'ATOM' or rec == 'HETATM' or rec == 'ANISOU':
+            if records[rec]['rec']:
+                sepdict.setdefault(
+                    rec, {i[2]: np.zeros(
+                        (rds[rec][0], ), dtype=i[3]
+                    ) for i in records[rec]['rec']}
+                )
+                slices = [(i[2], i[0], i[1]) for i in records[rec]['rec']]
+                for sub, i, j in slices:
+                    if line[i:j].strip():
+                        sepdict[rec][sub][rds[rec][1]] = line[i:j]
+                rds[rec][1] += 1
     for rec in sepdict:
         sepdict[rec] = pd.DataFrame.from_dict(sepdict[rec])
     return sepdict
