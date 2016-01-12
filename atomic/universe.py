f7da5e93ad9f1e624dcf43a2ab33b87f247345a3
Alex Marchenko <alexvmarch@gmail.com>
2016-01-12 18:11:12 -0500

    updated setup.py to account for db init

--- atomic/universe.py
+++ atomic/universe.py
@@ -4,7 +4,8 @@ Universe
 ====================
 The atomic container object.
 '''
-from exa.relational.container import Container, pkid, mapper_args
+from exa import Container
+from exa.relational.base import Column, Integer, ForeignKey
 #from atomic.one import One
 #from atomic.two import Two
 #from atomic.molecule import Molecule
@@ -17,13 +18,11 @@ class Universe(Container):
     be thought of as a snapshot in time. Each snaphot in time has information
     about atomic positions, energies, bond distances, etc.
     '''
-    pkid = pkid
-    __mapper_args__ = mapper_args
+    pkid = Column(Integer, ForeignKey('container.pkid'), primary_key=True)
+    frame_count = Column(Integer)
+    __mapper_args__ = {'polymorphic_identity': 'universe'}
 
     def __init__(self, one=None, two=None, molecule=None, **kwargs):
         '''
         '''
         super().__init__(**kwargs)
-        self.one = one
-        self.two = two
-        self.molecule = molecule
