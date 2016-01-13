626acc6a69127eb4c757d4bee17ea7cc84aa6280
Alex Marchenko <alexvmarch@gmail.com>
2016-01-13 13:13:28 -0500

    Updated twobody and atom

--- atomic/atom.py
+++ atomic/atom.py
@@ -9,11 +9,8 @@ from exa import DataFrame
 class Atom(DataFrame):
     '''
     Required indexes: frame, atom
-    
+
     Required columns: symbol, x, y, z
     '''
     __dimensions__ = ['frame', 'atom']
-    __attributes__ = ['symbol', 'x', 'y', 'z']
-
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
+    __columns__ = ['symbol', 'x', 'y', 'z']
