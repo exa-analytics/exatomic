9625a9e5db1e6b8bc0ac721704a5ebc901c804bb
Alex Marchenko <alexvmarch@gmail.com>
2016-01-24 12:35:08 -0500

    WIP new universe and dataframes

--- atomic/__init__.py
+++ atomic/__init__.py
@@ -1,17 +1,13 @@
 # -*- coding: utf-8 -*-
-import sys as _sys                              # HACK for development
-import os as _os
-_sys.path.insert(0, _os.sep.join(
-    (_os.path.dirname(_os.path.realpath(__file__)), '..', '..', 'exa'))
-)
+'''
+atomic
+===================
 
-
+'''
 __atomic_version__ = (0, 1, 0)                  # atomic VERSION NUMBER
 __version__ = '.'.join((str(v) for v in __atomic_version__))
 
 
-from exa import run_unittests, run_doctests     # imports from exa
-from exa import Dashboard
 from exa.relational.isotopes import Isotope
 from exa.relational.units import Length, Energy
 from exa.relational.constants import Constant
