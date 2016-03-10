4e55b624ebccabb9cf94eea15ef5418bff4c1c18
Alex <alexvmarch@gmail.com>
2016-03-10 16:39:04 -0500

    start of restructuring of sql

--- atomic/editor.py
+++ atomic/editor.py
@@ -12,6 +12,30 @@ class AtomicEditor(Editor):
     '''
     Editor specific to the atomic package.
     '''
+    @property
+    def atom_count(self):
+        '''
+        '''
+        if self._atom is None:
+            self.parse_atom()
+        return len(self._atom)
+
+    @property
+    def frame(self):
+        '''
+        '''
+        if self._frame is None:
+            self.parse_frame()
+        return self._frame
+
+    @property
+    def atom(self):
+        '''
+        '''
+        if self._atom is None:
+            self.parse_atom()
+        return self._atom
+
     def to_universe(self):
         '''
         Create a :class:`~atomic.universe.Universe` from the editor object.
@@ -20,3 +44,8 @@ class AtomicEditor(Editor):
         frame = self.frame
         meta = self.meta
         return Universe(frame=frame, atom=atom, meta=meta)
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self._atom = None
+        self._frame = None
