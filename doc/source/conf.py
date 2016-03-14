73e71904a349013c2fe4a9f52752a55d634fa065
Alex <alexvmarch@gmail.com>
2016-03-14 16:57:31 -0400

    Updated doc config to remove hidden/special by default

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -51,8 +51,8 @@ extensions = [
 # sphinx.ext.napoleon settings
 napoleon_google_docstring = True
 napoleon_numpy_docstring = False
-napoleon_include_private_with_doc = True
-napoleon_include_special_with_doc = True
+napoleon_include_private_with_doc = False
+napoleon_include_special_with_doc = False
 napoleon_use_admonition_for_examples = True
 napoleon_use_admonition_for_notes = True
 napoleon_use_admonition_for_references = True
