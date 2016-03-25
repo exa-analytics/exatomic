852b80dab5309ca1a96b049bcc03bbe793f21d34
Alex Marchenko <alexvmarch@gmail.com>
2016-03-24 23:11:00 -0400

    atom_ visualization JS

--- atomic/static/nbextensions/universe.js
+++ atomic/static/nbextensions/universe.js
@@ -38,6 +38,47 @@ define([
             */
             console.log('Initializing universe...');
             var self = this;
+            this.model.on('change:atom_x', this.update_x, this);
+            this.model.on('change:atom_y', this.update_y, this);
+            this.model.on('change:atom_z', this.update_z, this);
+            this.update_atom_x();
+            this.update_atom_y();
+            this.update_atom_z();
+            console.log(this.atom_x);
+            console.log(this.atom_y);
+            console.log(this.atom_z);
+            if (this.atom_z.length == undefined) {
+                console.log('true');
+            } else {
+                console.log('false');
+            };
+        },
+
+        update_atom_x: function() {
+            /*"""
+            update_atom_x
+            -----------
+            Updates x component of nuclear coordinates.
+            */
+            this.atom_x = this.get_trait('atom_x');
+        },
+
+        update_atom_y: function() {
+            /*"""
+            update_atom_y
+            -----------
+            Updates y component of nuclear coordinates.
+            */
+            this.atom_y = this.get_trait('atom_y');
+        },
+
+        update_atom_z: function() {
+            /*"""
+            update_atom_z
+            -----------
+            Updates z component of nuclear coordinates.
+            */
+            this.atom_z = this.get_trait('atom_z');
         },
     });
 
