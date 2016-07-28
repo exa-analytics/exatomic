f5f0c9f7bd26c5f3d9ec653f000563b0f9945004
Alex Marchenko <alexvmarch@gmail.com>
2016-07-28 17:27:52 -0400

    Revisions to tbp

--- exatomic/__init__.py
+++ exatomic/__init__.py
@@ -18,7 +18,7 @@ Warning:
 
 .. _atomic: https://en.wikipedia.org/wiki/Atomic_units
 """
-__exatomic_version__ = (0, 2, 11)
+__exatomic_version__ = (0, 2, 12)
 __version__ = '.'.join((str(v) for v in __exatomic_version__))
 
 
