4058cd7ff1c3406243a4da2018a790a6ad134595
Alex Marchenko <alexvmarch@gmail.com>
2017-02-22 08:55:52 -0800

    fixed typo in import

--- exatomic/qe/cp/error.py
+++ exatomic/qe/cp/error.py
@@ -3,7 +3,7 @@
 Errors, Exceptions, and Warnings for QE's cp.x Module
 =======================================================
 '''
-from extomic.qe.error import QEException
+from exatomic.qe.error import QEException
 
 
 class CPException(QEException):
