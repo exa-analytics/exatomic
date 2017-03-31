5e7d9ccd747c7640cb4aaaa3f6c3a069ee1c4ec0
tjduigna <thomas.j.duignan@gmail.com>
2017-03-30 20:28:46 -0600

    removed np. from exp in tests

--- exatomic/algorithms/test/test_orbital.py
+++ exatomic/algorithms/test/test_orbital.py
@@ -146,9 +146,9 @@ class TestGenBsfn(UnitTester):
 
     def test_gen_basfn(self):
         self.assertEqual(gen_basfn([''], self.uncontdf, self.atom['r2'], precision=0),
-                         '(1*np.exp(-1*(x**2+y**2+z**2)))')
+                         '(1*exp(-1*(x**2+y**2+z**2)))')
         self.assertEqual(gen_basfn([''], self.uncontdf, self.atom['r'], precision=1),
-                         '(1.0*np.exp(-1.0*(x**2+y**2+z**2)**0.5))')
+                         '(1.0*exp(-1.0*(x**2+y**2+z**2)**0.5))')
         self.assertEqual(gen_basfn([''], self.contdf, self.atom['r2'], precision=2),
-                         '(1.00*np.exp(-1.00*(x**2+y**2+z**2))+'
-                          '4.00*np.exp(-2.00*(x**2+y**2+z**2)))')
+                         '(1.00*exp(-1.00*(x**2+y**2+z**2))+'
+                          '4.00*exp(-2.00*(x**2+y**2+z**2)))')
