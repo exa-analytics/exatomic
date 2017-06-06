d812b41f1122c04a4ab87470bdcf67fd5009ac9c
Alex Marchenko <alexmarc@buffalo.edu>
2017-06-05 20:22:14 -0400

    ?

---
+++ exatomic/qe/tests/test_base_composer.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+# Copyright (c) 2015-2017, Exa Analytics Development Team
+# Distributed under the terms of the Apache License 2.0
+"""
+Tests for :mod:`~exatomic.qe.base_composer`
+##########################################
+"""
+from unittest import TestCase
+from exatomic.qe.base_composer import PWCPComposer
+
