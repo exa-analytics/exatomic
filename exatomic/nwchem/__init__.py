cf7256ed26fa529bc2888275e70a24585d882446
Alex Marchenko <alexmarc@buffalo.edu>
2017-10-24 14:48:21 -0400

    WIP nwchem parsers update

--- exatomic/nwchem/__init__.py
+++ exatomic/nwchem/__init__.py
@@ -1,6 +1,16 @@
 # -*- coding: utf-8 -*-
 # Copyright (c) 2015-2017, Exa Analytics Development Team
 # Distributed under the terms of the Apache License 2.0
-from .editor import Editor
-from .inputs import Input, tuning_inputs
-from .output import Output, parse_nwchem
+"""
+Support for `NWChem`_
+###########################
+This sub-package is organized in terms of the various 'tasks' supported by NWChem.
+A description of the tasks can be found on the NWChem `wiki`_.
+
+.. _NWChem: www.nwchem-sw.org
+.. _wiki: http://www.nwchem-sw.org/index.php/Release66:Top-level
+"""
+
+#from .editor import Editor
+#from .inputs import Input, tuning_inputs
+#from .output import Output, parse_nwchem
