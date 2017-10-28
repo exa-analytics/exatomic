9e29bb7648c549cccb2dfce2f9cb7ac5cfca3b80
Alex Marchenko <alexvmarch@gmail.com>
2017-10-28 11:45:43 -0400

    Expanded build channels for symengine

--- exatomic/base.py
+++ exatomic/base.py
@@ -54,14 +54,14 @@ def resource(name):
 
 
 # Element mappers
-isotopedf = isotopes.as_df()
-sym2z = isotopedf.drop_duplicates("symbol").set_index("symbol")["Z"].to_dict()
+elements = isotopes.df.drop_duplicates("symbol").sort_values("symbol")
+sym2z = elements.set_index("symbol")["Z"].to_dict()
 z2sym = {v: k for k, v in sym2z.items()}
 sym2mass = {}
 sym2radius = {}
 sym2color = {}
-for k, v in vars(isotopes).items():
-    if isinstance(v, isotopes.Element):
-        sym2mass[k] = v.mass
-        sym2radius[k] = v.radius
-        sym2color[k] = v.color
+for element in sym2z.keys():
+    ele = isotopes.get(element)
+    sym2mass[element] = ele.mass
+    sym2radius[element] = ele.radius
+    sym2color[element] = ele.color
