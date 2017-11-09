89dc905ea7db9f9376df146641adcf11ba760d5e
tjduigna <thomas.j.duignan@gmail.com>
2017-11-09 09:06:00 -0500

    Finalizing active scenes WIP

--- exatomic/widget_base.py
+++ exatomic/widget_base.py
@@ -14,19 +14,18 @@ from traitlets import (Bool, Int, Float, Unicode,
 from ipywidgets import (
     Box, VBox, HBox, FloatSlider, IntSlider, Play, Text,
     IntRangeSlider, DOMWidget, Layout, Button, Dropdown,
-    register, jslink
+    register, jslink, ToggleButton
     # Unused = HBox, FloatSlider,
 )
-from exatomic import __js_version__
-from .widget_utils import (_glo, _flo, _wlo, _hboxlo, _vboxlo,
+from exatomic import Universe, __js_version__
+from .traits import uni_traits
+from .widget_utils import (_glo, _flo, _wlo, _hboxlo, _vboxlo, _bboxlo,
                            _ListDict, _scene_grid,
                            Folder, GUIBox, gui_field_widgets)
 
 
 @register
 class ExatomicScene(DOMWidget):
-
-
     _model_module_version = Unicode(__js_version__).tag(sync=True)
     _model_module_version = Unicode(__js_version__).tag(sync=True)
     _view_module = Unicode("jupyter-exatomic").tag(sync=True)
@@ -123,16 +122,50 @@ class ExatomicScene(DOMWidget):
     def __init__(self, *args, **kwargs):
         lo = kwargs.pop('layout', None)
         if lo is None:
-            height = kwargs.pop('height', '100%')
+            height = kwargs.pop('height', 'auto')
             min_height = kwargs.pop('min_height', '400px')
             min_width = kwargs.pop('min_width', '400px')
             flex = kwargs.pop('flex', '1 1 auto')
             lo = Layout(height=height, min_height=min_height,
                         flex=flex, min_width=min_width)
-            print(lo)
         super(DOMWidget, self).__init__(
             *args, layout=lo, **kwargs)
 
+
+
+@register
+class UniverseScene(ExatomicScene):
+    """A scene for viewing quantum systems."""
+    _model_name = Unicode("UniverseSceneModel").tag(sync=True)
+    _view_name = Unicode("UniverseSceneView").tag(sync=True)
+    # Top level index
+    frame_idx = Int(0).tag(sync=True)
+    axis = Bool(False).tag(sync=True)
+    # Atom traits
+    atom_x = Unicode().tag(sync=True)
+    atom_y = Unicode().tag(sync=True)
+    atom_z = Unicode().tag(sync=True)
+    atom_l = Unicode().tag(sync=True)
+    atom_s = Unicode().tag(sync=True)
+    atom_r = Dict().tag(sync=True)
+    atom_c = Dict().tag(sync=True)
+    atom_3d = Bool(False).tag(sync=True)
+    # Two traits
+    two_b0 = Unicode().tag(sync=True)
+    two_b1 = Unicode().tag(sync=True)
+    # Field traits
+    field_i = List().tag(sync=True)
+    field_v = Unicode().tag(sync=True)
+    field_p = Dict().tag(sync=True)
+    field_idx = Any().tag(sync=True)
+    field_iso = Float(0.03).tag(sync=True)
+    field_show = Bool(False).tag(sync=True)
+    cont_show = Bool(False).tag(sync=True)
+    cont_axis = Unicode("z").tag(sync=True)
+    cont_num = Int(10).tag(sync=True)
+    cont_lim = List([-8, -1]).tag(sync=True)
+    cont_val = Float(0.0).tag(sync=True)
+    # Frame traits
 
 
 
@@ -149,6 +182,13 @@ class ExatomicBox(Box):
     active_scene_indices = List().tag(sync=True)
     linked = Bool(False).tag(sync=True)
 
+    def _update_active(self, b):
+        self.active_scene_indices = []
+        for i, (key, obj) in enumerate(self._controls['active']._controls.items()):
+            if key == 'main': continue
+            if obj.value:
+                self.active_scene_indices.append(i - 1)
+
     def _close(self, b):
         """Shut down all active widgets within the container."""
 
@@ -164,10 +204,19 @@ class ExatomicBox(Box):
         mit = self._controls.values()
         return [obj for obj in mit if obj.active]
 
+    def _active_folder(self):
+        active = Button(icon='bars', description=' Active Scenes')
+        opts = _ListDict([
+            (str(i), ToggleButton(description=str(i), value=True))
+            for i, scn in enumerate(self.scenes)])
+        for key, obj in opts.items():
+            obj.observe(self._update_active, names='value')
+        return Folder(active, opts)
+
     def _save_folder(self):
         saves = Button(icon='save', description=' Image')
         saveopts = _ListDict([
-            ('dir', Text(value='directory')),
+            ('dir', Text(value=os.getcwd())),
             ('name', Text(value='name')),
             ('save', Button(icon='download', description=' Save'))
         ])
@@ -175,6 +224,7 @@ class ExatomicBox(Box):
             link((saveopts['dir'], 'value'), (scn, 'savedir'))
             link((saveopts['name'], 'value'), (scn, 'imgname'))
         def _saves(b):
+            # self._update_active()
             for idx in self.active_scene_indices:
                 self.scenes[idx].save = self.scenes[idx].save == False
         saveopts['save'].on_click(_saves)
@@ -189,6 +239,7 @@ class ExatomicBox(Box):
              ('set', IntSlider(description='Load', min=-1,
                                max=ncams-1, value=-1, step=1))])
         def _save_cam(b):
+            # self._update_active()
             for idx in self.active_scene_indices:
                 scn = self.scenes[idx]
                 scn.save_cam = scn.save_cam == False
@@ -214,18 +265,23 @@ class ExatomicBox(Box):
     def _field_folder(self):
         fdict = gui_field_widgets(self.uni, self.test)
         def _iso(c):
+            # self._update_active()
             for idx in self.active_scene_indices:
                 self.scenes[idx].field_iso = c.new
         def _alpha(c):
+            # self._update_active()
             for idx in self.active_scene_indices:
                 self.scenes[idx].field_o = c.new
         def _nx(c):
+            # self._update_active()
             for idx in self.active_scene_indices:
                 self.scenes[idx].field_nx = c.new
         def _ny(c):
+            # self._update_active()
             for idx in self.active_scene_indices:
                 self.scenes[idx].field_ny = c.new
         def _nz(c):
+            # self._update_active()
             for idx in self.active_scene_indices:
                 self.scenes[idx].field_nz = c.new
         fdict['iso'].observe(_iso, names='value')
@@ -239,7 +295,7 @@ class ExatomicBox(Box):
 
         return folder
 
-    def _init_gui(self):
+    def _init_gui(self, **kwargs):
         """Initialize generic GUI controls and register callbacks."""
 
         mainopts = _ListDict([
@@ -248,46 +304,51 @@ class ExatomicBox(Box):
         mainopts['close'].on_click(self._close)
         if self.scenes:
             def _clear(b):
+                # self._update_active()
                 for idx in self.active_scene_indices:
                     self.scenes[idx].clear = self.scenes[idx].clear == False
             mainopts['close'].on_click(self._close)
             mainopts['clear'].on_click(_clear)
 
-        mainopts.update([('saves', self._save_folder())])
-        mainopts.update([('camera', self._camera_folder())])
+        mainopts.update([('active', self._active_folder()),
+                         ('saves', self._save_folder()),
+                         ('camera', self._camera_folder())])
         return mainopts
 
 
-    def __init__(self, scenes, **kwargs):
+    def __init__(self, *objs, **kwargs):
         if not hasattr(self, 'uni'):
             self.uni = kwargs.pop('uni', False)
         if not hasattr(self, 'test'):
             self.test = kwargs.pop('test', True)
-        if isinstance(scenes, DOMWidget):
-            scenes.uni = self.uni
-            scenes.test = self.test
-            self.scenes = [scenes]
-            scenes = VBox([HBox([scenes], layout=_hboxlo)], layout=_vboxlo)
-        else:
-            mh = kwargs.pop('min_height', None)
-            mw = kwargs.pop('min_width', None)
-            unis, grid = _scene_grid(scenes, min_height=mh, min_width=mw)
-            scenes = VBox([HBox([ExatomicScene(**scn)
-                           for scn in row], layout=_hboxlo)
-                           for row in grid], layout=_vboxlo)
-            self.scenes = []
-            for scn in scenes.children:
-                for sub in scn.children:
-                    sub.uni = self.uni
-                    sub.test = self.test
-                    self.scenes.append(sub)
-        self._controls = self._init_gui()
+        mh = kwargs.pop('min_height', None)
+        mw = kwargs.pop('min_width', None)
+        objs, grid = _scene_grid(objs, min_height=mh, min_width=mw)
+        self.scenes, scenes = [], []
+        exists = not isinstance(objs[0], int)
+        cnt = 0
+        for rows in grid:
+            scenes.append([])
+            for kws in rows:
+                if exists:
+                    kws.update(objs[cnt])
+                else: kws.update({'uni': self.uni, 'test': self.test})
+                if kws['uni'] and not kws['test']:
+                    obj = UniverseScene(**kws)
+                else:
+                    obj = ExatomicScene(**kws)
+                self.scenes.append(obj)
+                scenes[-1].append(obj)
+                cnt += 1
+        scenes = VBox([HBox(scns, layout=_hboxlo)
+                       for scns in scenes], layout=_vboxlo)
+        self._controls = self._init_gui(nframes=kwargs.pop('nframes', None),
+                                        fields=kwargs.pop('fields', None))
         for key, obj in self._controls.items():
             if not hasattr(obj, 'active'): obj.active = True
         self.active_scene_indices = list(range(len(self.scenes)))
         lo = kwargs.pop('layout', None)
         gui = GUIBox(self._get())
-        if self.scenes:
-            super(ExatomicBox, self).__init__([gui, scenes], **kwargs)
-        else:
-            super(ExatomicBox, self).__init__([gui], **kwargs)
+        children = [gui, scenes] if self.scenes else [gui]
+        super(ExatomicBox, self).__init__(
+                children, layout=_bboxlo, **kwargs)
