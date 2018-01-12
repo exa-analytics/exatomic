57964cb4faf7c3aef1d0d022ba06de083b6f39cc
Alex Marchenko <alexvmarch@gmail.com>
2018-01-12 15:50:21 -0500

    Updated build requirements, build scripts, and conda build yaml

--- bld.bat
+++ bld.bat
@@ -1,3 +1,3 @@
-"%PYTHON%" setup.py install
+"%PYTHON%" setup.py install --single-version-externally-managed --record=record.txt
 jupyter nbextension enable exatomic --py --sys-prefix
 if errorlevel 1 exit 1
