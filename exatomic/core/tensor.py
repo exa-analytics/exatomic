c8877aecae3d2e13dad7367f03bed6a9a3dbbeeb
Herbert D Ludowieg <herbertludowieg@github.com>
2018-02-28 14:50:07 -0500

    Deleting unused code

--- exatomic/core/tensor.py
+++ exatomic/core/tensor.py
@@ -43,21 +43,6 @@ class Tensor(DataFrame):
                                                     'frame','atom','label']
     _categories = {'frame': np.int64, 'label': str}
 
-#    def _get_eigen(self, n, df):
-#        value = np.zeros((n,3))
-#        vector = np.zeros((n,3,3))
-#        for i in range(n):
-#            A = df.loc[df.groupby([0,1,2])['grp'].filter(lambda x: 
-#                                            x.sum()==i).index]
-#            eigen = np.linalg.eig(A.values[:,[0,1,2]])
-#            value[i] = eigen[0]
-#            vector[i] = eigen[1]
-#        valuedf = pd.DataFrame(data = np.transpose(value))
-#        vectordf = pd.DataFrame(data = vector[0])
-#        print(valuedf,vectordf)
-#        return valuedf
-
-
     @classmethod
     def from_file(cls, filename):
         """
@@ -88,7 +73,6 @@ class Tensor(DataFrame):
         df = df[~df.index.isin(idxs)]
         df[1] = df[1].astype(np.float64)
         df['grp'] = [i for i in range(n) for j in range(3)]
-#        eigen = cls._get_eigen(cls, n, df)
         df = pd.DataFrame(df.groupby('grp').apply(lambda x: 
                      x.unstack().values[:-3]).values.tolist(),
                      columns=['xx','xy','xz','yx','yy','yz','zx','zy','zz'])
