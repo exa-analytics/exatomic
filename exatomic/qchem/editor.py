2226329278c710e330f354642359b9fedc217e29
Herbert D. Ludowieg <herbertl@buffalo.edu>
2018-07-21 10:13:25 -0400

    Initial commit for Q-Chem output reader and input generator

---
+++ exatomic/qchem/editor.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+# Copyright (c) 2015-2018, Exa Analytics Development Team
+# Distributed under the terms of the Apache License 2.0
+"""
+Gaussian Editor
+#######################
+Base class on top of exatomic.Editor for Gaussian Editors
+"""
+#import pandas as pd
+from exatomic import Editor
+
+class Editor(Editor):
+
+    def __init__(self, *args, **kwargs):
+        super(Editor, self).__init__(*args, **kwargs)
+        if self.meta is None: self.meta = {'program': 'qchem',
+                                           'qchem': True}
+        else: self.meta.update({'program': 'qchem',
+                                'qchem': True})
