97a56739afda5787682a3e4d03ccc1e0be6ca022
Herbert Ludowieg <herbertl@buffalo.edu>
2019-05-29 12:19:27 -0400

    Generalizing the code a bit

--- exatomic/adf/nmr/output.py
+++ exatomic/adf/nmr/output.py
@@ -35,34 +35,45 @@ class Output(six.with_metaclass(OutMeta, Editor)):
     def parse_atom(self):
         _reatom = "NUCLEAR COORDINATES (ANGSTROMS):"
         found = self.find(_reatom, keys_only=True)
-        if len(found) > 1:
-            raise NotImplementedError("We can only parse outputs from a single NMR calculation")
-        start = found[0] + 3
-        stop = start
-        while self[stop].strip(): stop += 1
-        # a bit of a hack to make sure that there is no formatting change depending on the
-        # number of atoms in the molecule as the index is right justified so if there are
-        # more than 100 atoms it will fill the alloted space for the atom index and change the
-        # delimitter and therefore the number of columns
-        self[start:stop] = map(lambda x: x.replace('(', ''), self[start:stop])
-        atom = self.pandas_dataframe(start, stop, ncol=5)
-        #atom.drop(1, axis='columns', inplace=True)
-        atom.columns = ['symbol', 'set', 'x', 'y', 'z']
-        for c in ['x', 'y', 'z']: atom[c] *= Length['Angstrom', 'au']
-        atom['Z'] = atom['symbol'].map(sym2z)
-        atom['frame'] = 0
-        atom['set'] = list(map(lambda x: x.replace('):', ''), atom['set']))
-        atom['set'] = atom['set'].astype(int)
-        self.atom = atom
+        #if len(found) > 1:
+        #    raise NotImplementedError("We can only parse outputs from a single NMR calculation")
+        atom = []
+        for idx, val in enumerate(found):
+            start = val + 3
+            stop = start
+            while self[stop].strip(): stop += 1
+            # a bit of a hack to make sure that there is no formatting change depending on the
+            # number of atoms in the molecule as the index is right justified so if there are
+            # more than 100 atoms it will fill the alloted space for the atom index and change the
+            # delimitter and therefore the number of columns
+            self[start:stop] = map(lambda x: x.replace('(', ''), self[start:stop])
+            df = self.pandas_dataframe(start, stop, ncol=5)
+            #df.drop(1, axis='columns', inplace=True)
+            df.columns = ['symbol', 'set', 'x', 'y', 'z']
+            for c in ['x', 'y', 'z']: df[c] *= Length['Angstrom', 'au']
+            df['Z'] = df['symbol'].map(sym2z)
+            df['frame'] = idx
+            df['set'] = list(map(lambda x: x.replace('):', ''), df['set']))
+            df['set'] = df['set'].astype(int) - 1
+            atom.append(df)
+        self.atom = pd.concat(atom, ignore_index=True)
 
     def parse_nmr_shielding(self):
         _reatom = "N U C L E U S :"
         _reshield = "==== total shielding tensor"
+        _renatom = "NUCLEAR COORDINATES (ANGSTROMS)"
         found = self.find(_reatom, keys_only=True)
         if not found:
             raise NotImplementedError("Could not find {} in output".format(_reatom))
+        ncalc = self.find(_renatom, keys_only=True)
+        ncalc.append(len(self))
+        ndx = 0
         dfs = []
         for start in found:
+            try:
+                ndx = ndx if start > ncalc[ndx] and start < ncalc[ndx+1] else ndx+1
+            except IndexError:
+                raise IndexError("It seems that there was an issue with determining which NMR calculation we are in")
             start_shield = self.find(_reshield, keys_only=True, start=start)[0] + start + 2
             end_shield = start_shield + 3
             symbol, index = self[start].split()[-1].split('(')
@@ -72,10 +83,10 @@ class Output(six.with_metaclass(OutMeta, Editor)):
             cols = ['xx', 'xy', 'xz', 'yx', 'yy', 'yz', 'zx', 'zy', 'zz']
             df = pd.DataFrame(df.unstack().values.reshape(1,9), columns=cols)
             df['isotropic'] = isotropic
-            df['atom'] = index
+            df['atom'] = index - 1
             df['symbol'] = symbol
             df['label'] = 'nmr shielding'
-            df['frame'] = 0
+            df['frame'] = ndx
             dfs.append(df)
         shielding = pd.concat(dfs, ignore_index=True)
         self.nmr_shielding = shielding
