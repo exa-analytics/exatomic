b4fd6ae0abc8baccfa05ef83211006c04f11f260
Herbert Ludowieg <herbertl@buffalo.edu>
2019-06-11 12:39:02 -0400

    Adding frequency and gradient tests

--- exatomic/adf/tests/test_output.py
+++ exatomic/adf/tests/test_output.py
@@ -16,6 +16,7 @@ class TestADFOutput(TestCase):
         # TODO :: File with excitation
         self.pf3 = Output(resource('adf-pf3-nmr.out'))
         self.c2h2 = Output(resource('adf-c2h2-cpl.out'))
+        self.ch4 = Ouput(resource('adf-ch4-opt-freq.out'))
 
     def test_parse_atom(self):
         self.lu.parse_atom()
@@ -55,13 +56,23 @@ class TestADFOutput(TestCase):
         self.assertEqual(self.lu.orbital.shape[0], 20)
         self.assertTrue(np.all(pd.notnull(self.lu.orbital)))
 
-    def test_nmr_shielding(self):
+    def test_parse_nmr_shielding(self):
         self.pf3.parse_nmr_shielding()
         self.assertEqual(self.pf3.nmr_shielding.shape[0], 4)
         self.assertTrue(np.all(pd.notnull(self.pf3.nmr_shielding)))
 
-    def test_j_coupling(self):
+    def test_parse_j_coupling(self):
         self.c2h2.parse_j_coupling()
         self.assertEqual(self.c2h2.j_coupling.shape[0], 4)
         self.assertTrue(np.all(pd.notnull(self.c2h2.j_coupling)))
+
+    def test_parse_frequency(self):
+        self.ch4.parse_frequency()
+        self.assertEqual(self.ch4.frequency.shape[0], 45)
+        self.assertTrue(np.all(pd.notnull(self.ch4.frequency)))
+
+    def test_parse_gradient(self):
+        self.ch4.parse_gradient()
+        self.assertEqual(self.ch4.gradient.shape[0], 15)
+        self.assertTrue(np.all(pd.notnull(self.ch4.gradient)))
 
