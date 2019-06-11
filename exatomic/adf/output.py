d170bd773b328ab745b988f227aba029e43ac712
Herbert Ludowieg <herbertl@buffalo.edu>
2019-06-11 11:24:17 -0400

    Adding NMR parsers to editor

--- exatomic/adf/output.py
+++ exatomic/adf/output.py
@@ -24,6 +24,7 @@ from exatomic.core.atom import Atom, Frequency
 from exatomic.core.gradient import Gradient
 from exatomic.core.basis import BasisSet, BasisSetOrder
 from exatomic.core.orbital import Orbital, Excitation, MOMatrix
+from exatomic.core.tensor import NMRShielding, JCoupling
 from .editor import Editor
 
 
@@ -38,7 +39,8 @@ class OutMeta(TypedMeta):
     sphr_momatrix = MOMatrix
     gradient = Gradient
     frequency = Frequency
-
+    nmr_shielding = NMRShielding
+    j_coupling = JCoupling
 
 class Output(six.with_metaclass(OutMeta, Editor)):
     """The ADF output parser."""
@@ -419,5 +421,78 @@ class Output(six.with_metaclass(OutMeta, Editor)):
         # TODO: check units of the normal modes
         self.frequency = frequency
 
+    def parse_nmr_shielding(self):
+        _reatom = "N U C L E U S :"
+        _reshield = "==== total shielding tensor"
+        _renatom = "NUCLEAR COORDINATES (ANGSTROMS)"
+        found = self.find(_reatom, keys_only=True)
+        if not found:
+            #raise NotImplementedError("Could not find {} in output".format(_reatom))
+            return
+        ncalc = self.find(_renatom, keys_only=True)
+        ncalc.append(len(self))
+        ndx = 0
+        dfs = []
+        for start in found:
+            try:
+                ndx = ndx if start > ncalc[ndx] and start < ncalc[ndx+1] else ndx+1
+            except IndexError:
+                raise IndexError("It seems that there was an issue with determining which NMR calculation we are in")
+            start_shield = self.find(_reshield, keys_only=True, start=start)[0] + start + 2
+            end_shield = start_shield + 3
+            symbol, index = self[start].split()[-1].split('(')
+            index = int(index.replace(')', ''))
+            isotropic = float(self[start_shield+4].split()[-1])
+            df = self.pandas_dataframe(start_shield, end_shield, ncol=3)
+            cols = ['xx', 'xy', 'xz', 'yx', 'yy', 'yz', 'zx', 'zy', 'zz']
+            df = pd.DataFrame(df.unstack().values.reshape(1,9), columns=cols)
+            df['isotropic'] = isotropic
+            df['atom'] = index - 1
+            df['symbol'] = symbol
+            df['label'] = 'nmr shielding'
+            df['frame'] = ndx
+            dfs.append(df)
+        shielding = pd.concat(dfs, ignore_index=True)
+        self.nmr_shielding = shielding
+
+    def parse_j_coupling(self):
+        _recoupl = "total calculated spin-spin coupling:"
+        _reatom = "Internal CPL numbering of atoms:"
+        found = self.find(_reatom, keys_only=True)
+        if not found:
+            return
+        found = self.find(_reatom, _recoupl, keys_only=True)
+        # we grab the tensors inside the principal axis representation
+        # for the cartesian axis representation we start the list at 0 and grab every other instance
+        start_coupl = found[_recoupl][1::2]
+        start_pert = np.array(found[_reatom]) - 3
+        dfs = []
+        # grab atoms
+        cols = ['xx', 'xy', 'xz', 'yx', 'yy', 'yz', 'zx', 'zy', 'zz']
+        for ln, start in zip(start_pert, start_coupl):
+            line = self[ln].split()
+            # we just replace all of the () in the strings
+            pert_nucl = list(map(lambda x: x.replace('(', '').replace(')', ''), line[5:]))
+            nucl = list(map(lambda x: x.replace('(', '').replace(')', ''), line[1:3]))
+            # grab both tensors
+            df = self.pandas_dataframe(start+2, start+5, ncol=6)
+            # this will grab the iso value and tensor elements for the j coupling in hz
+            df.drop(range(3), axis='columns', inplace=True)
+            df = pd.DataFrame(df.unstack().values.reshape(1,9), columns=cols)
+            iso = self[start+1].split()[-1]
+            # place all of the dataframe columns
+            df['isotropic'] = float(iso)
+            df['atom'] = int(nucl[0])
+            df['symbol'] = nucl[1]
+            df['pt_atom'] = int(pert_nucl[0])
+            df['pt_symbol'] = pert_nucl[1]
+            df['label'] = 'j coupling'
+            df['frame'] = 0
+            dfs.append(df)
+        # put everything together
+        j_coupling = pd.concat(dfs, ignore_index=True)
+        self.j_coupling = j_coupling
+
     def __init__(self, *args, **kwargs):
         super(Output, self).__init__(*args, **kwargs)
+
