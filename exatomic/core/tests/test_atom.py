0b53b01e94c3f8dd7326e2af90bc802d83a213f9
Herbert Ludowieg <herbertl@buffalo.edu>
2019-08-13 09:39:55 -0400

    Adding test for centering to the center of nuclear charge and adding some comments in tests

--- exatomic/core/tests/test_atom.py
+++ exatomic/core/tests/test_atom.py
@@ -11,16 +11,29 @@ class TestAtom(TestCase):
     def setUp(self):
         self.h2o = XYZ(resource('H2O.xyz'))
         self.h2o.parse_atom()
+        self.h2 = XYZ(resource('H2.xyz'))
+        self.h2.parse_atom()
         self.cols = ['x', 'y', 'z']
 
     def test_center(self):
-        centered = self.h2o.atom.center(0)
+        # test vanilla centering algorithm
+        centered = self.h2o.atom.center(idx=0)
         center = np.array([[ 0.        ,  0.        ,  0.        ],
                            [ 1.86954385, -0.05270446,  0.06149169],
                            [-0.54683005, -1.16477049,  1.35871308]])
-        self.assertTrue(np.allclose(centered[['x','y','z']].values, center))
+        self.assertTrue(np.allclose(centered[self.cols].values, center))
+        # test center of nuclear charge algorithm with a displaced H2 molecue
+        center_nc = np.array([[-0.66832999,  0.02918682,  0.        ],
+                              [ 0.66832999, -0.02918682,  0.        ]])
+        centered = self.h2.atom.center(idx=0, to='NuclChrg')
+        self.assertTrue(np.allclose(centered[self.cols].values, center_nc))
+        # make sure we raise an error when a centering method that has not been implemented
+        # is given
+        with self.assertRaises(NotImplementedError):
+           self.h2.atom.center(idx=0, to='UnknownCenteringMethod')
 
     def test_translate(self):
+        # displace in each coordinate direction by 0.1 Bohr
         disp = 0.1
         trans_x = self.h2o.atom.translate(dx=disp)
         self.assertTrue(np.allclose(trans_x['x'].values, self.h2o.atom['x']+disp))
@@ -28,6 +41,7 @@ class TestAtom(TestCase):
         self.assertTrue(np.allclose(trans_y['y'].values, self.h2o.atom['y']+disp))
         trans_z = self.h2o.atom.translate(dz=disp)
         self.assertTrue(np.allclose(trans_z['z'].values, self.h2o.atom['z']+disp))
+        # displace by a vector dx=1, dy=2.5, dz=1 Bohr
         trans_vec = self.h2o.atom.translate(vector=[1, 2.5, 1])
         vec_data = np.array([[-9.07214576,  3.6018993 ,  0.96396292],
                              [-7.20260191,  3.54919484,  1.02545461],
@@ -35,18 +49,30 @@ class TestAtom(TestCase):
         self.assertTrue(np.allclose(trans_vec[self.cols].values, vec_data))
 
     def test_rotate(self):
+        # rotate 135 degrees
         rot_data = np.array([[6.342922101, -7.901243041, -0.036037077],
                              [5.058222648, -6.542008224,  0.025454611],
                              [7.553206450, -7.464293163,  1.322676007]])
         rotated = self.h2o.atom.rotate(theta=135.0)
         self.assertTrue(np.allclose(rotated[self.cols].values, rot_data))
+        # rotate 0 degrees
+        # should return the same coordinates
+        rotated = self.h2o.atom.rotate(theta=0)
+        self.assertTrue(np.allclose(rotated[self.cols].values, self.h2o.atom.last_frame[self.cols]))
 
     def test_align(self):
-        align_data = np.array([[ 0.0000000000, 0.0000000000, 0.0000000000],
-                               [ 1.8712970607, 0.0000000000,-0.0000000000],
+        # align to x axis
+        align_data = np.array([[ 0.          , 0.          , 0.          ],
+                               [ 1.8712970607, 0.          ,-0.          ],
                                [-0.4688643421,-1.1790804580, 1.3754088998]])
         aligned = self.h2o.atom.align(adx0=0, adx1=1, axis=[1,0,0])
         self.assertTrue(np.allclose(aligned[self.cols].values, align_data))
+        # test aligning alog z axis and centering to the cnc
+        align_nc = np.array([[0., 0., -0.668966999],
+                             [0., 0.,  0.668966999]])
+        aligned = self.h2.atom.align(adx0=0, adx1=1, axis=[0, 0, 1], center_to='NuclChrg')
+        print(aligned[self.cols].values)
+        self.assertTrue(np.allclose(aligned[self.cols].values, align_nc))
 
 
 #"""
