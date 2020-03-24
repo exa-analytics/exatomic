c7d81d3004918ed1a224d4d7fab47b9e08c652e5
Herbert D Ludowieg <hbr691@gmail.com>
2020-03-24 12:54:41 -0400

    Updating test

--- exatomic/tests/test_base.py
+++ exatomic/tests/test_base.py
@@ -4,13 +4,12 @@
 from exatomic.base import sym2isomass
 import pytest
 
-params = [(['Ni'], None, [57.93534241]),
-          (['Ni', 'H', 'C'], None, [57.93534241, 1.00782503223, 12.0000]),
-          (['Ni', 'H', 'C'], [64, 2, 13], [63.92796682, 2.01410177812, 13.00335483507])]
+params = [(['Ni'], [57.93534241]),
+          (['Ni', 'H', 'C'], [57.93534241, 1.00782503223, 12.0000])]
 
-@pytest.mark.parametrize("symbol, isotope, expected", params)
-def test_sym2isomass(symbol, isotope, expected):
-    dict = sym2isomass(symbol, isotope)
+@pytest.mark.parametrize("symbol, expected", params)
+def test_sym2isomass(symbol, expected):
+    dict = sym2isomass(symbol)
     close = []
     for symb, exp in zip(symbol, expected):
         close.append(abs(dict[symb] - exp) < 1e-6)
