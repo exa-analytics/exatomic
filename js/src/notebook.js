66c8b965a92cb4437ad262d26711c6b8601aba13
tjduigna <thomas.j.duignan@gmail.com>
2020-07-04 13:36:58 -0400

    var

--- js/src/notebook.js
+++ js/src/notebook.js
@@ -1,3 +1,3 @@
-__webpack_public_path__ = document.querySelector("body").getAttribute("data-base-url") + "nbextensions/exatomic/";
+var __webpack_public_path__ = document.querySelector("body").getAttribute("data-base-url") + "nbextensions/exatomic/";
 
 module.exports = require("./index.js");
