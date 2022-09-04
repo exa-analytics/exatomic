ced910c813786ba1a64668398e6bb8e18e04f7a0
Herbert D Ludowieg <herbertl@buffalo.edu>
2022-09-04 18:18:20 -0400

    Documentation things

--- exatomic/adf/output.py
+++ exatomic/adf/output.py
@@ -46,8 +46,8 @@ class OutMeta(TypedMeta):
 
 class NMR(six.with_metaclass(OutMeta, Editor)):
     def parse_atom(self):
-        # use the regex instead of find because we have a similar search string in an nmr and
-        # cpl calculation for the nuclear coordinates
+        # use the regex instead of find because we have a similar search
+        # string in an nmr and cpl calculation for the nuclear coordinates
         _reatom = "(?i)NUCLEAR COORDINATES"
         found = self.regex(_reatom, keys_only=True)
         atom = []
@@ -55,10 +55,11 @@ class NMR(six.with_metaclass(OutMeta, Editor)):
             start = val + 3
             stop = start
             while self[stop].strip(): stop += 1
-            # a bit of a hack to make sure that there is no formatting change depending on the
-            # number of atoms in the molecule as the index is right justified so if there are
-            # more than 100 atoms it will fill the alloted space for the atom index and change the
-            # delimitter and therefore the number of columns
+            # a bit of a hack to make sure that there is no formatting change
+            # depending on the number of atoms in the molecule as the index is
+            # right justified so if there are more than 100 atoms it will fill
+            # the alloted space for the atom index and change the delimitter
+            # and therefore the number of columns
             self[start:stop] = map(lambda x: x.replace('(', ''), self[start:stop])
             df = self.pandas_dataframe(start, stop, ncol=5)
             df.columns = ['symbol', 'set', 'x', 'y', 'z']
@@ -73,6 +74,7 @@ class NMR(six.with_metaclass(OutMeta, Editor)):
         self.atom = atom
 
     def parse_nmr_shielding(self):
+        ''' Parse the NMR shielding in the cartesian axis represntation. '''
         _reatom = "N U C L E U S :"
         _reshield = "==== total shielding tensor"
         _renatom = "NUCLEAR COORDINATES (ANGSTROMS)"
@@ -88,8 +90,10 @@ class NMR(six.with_metaclass(OutMeta, Editor)):
             try:
                 ndx = ndx if start > ncalc[ndx] and start < ncalc[ndx+1] else ndx+1
             except IndexError:
-                raise IndexError("It seems that there was an issue with determining which NMR calculation we are in")
-            start_shield = self.find(_reshield, keys_only=True, start=start)[0] + start + 2
+                raise IndexError("It seems that there was an issue with " \
+                                 +"determining which NMR calculation we are in.")
+            start_shield = self.find(_reshield, keys_only=True, start=start)[0] \
+                           + start + 2
             end_shield = start_shield + 3
             symbol, index = self[start].split()[-1].split('(')
             index = int(index.replace(')', ''))
